6074fc975bba0babb3ea64d248fc1904354dfbfa
domenico <domenico@element.in>
2021-09-06 15:53:46 +0200

    importing antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-// We use Route in order to define the different routes of our application
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// We import all the components we need in our app
 import RecordList from "./components/recordList";
-import "./App.css";
 import { Navbar } from "./components/navbar";
 import EditSchema from "./components/edit";
+import 'antd/dist/antd.css';
+import "./App.css";
 
 //TODO repackage to each docker container
-//TODO fix creation and editing UI styles through ant design
 //TODO add proper logging
 //TODO adapt for more entities
 //TODO create generic testing for client and server through jest/react-testing-library
